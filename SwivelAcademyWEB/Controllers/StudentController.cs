c7adbb5d963796cbeddbe104150daa485ed430c3
Chidiebere Nwaneto <cnwaneto@axamansard.com>
2021-06-25 16:13:38 +0100

    Added the Profile creation pages for teachers

---
+++ SwivelAcademyWEB/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SwivelAcademyWEB.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SwivelAcademyWEB.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        public static IConfiguration _configuration { get; set; }
+        private readonly ISRepository _sRepo;
+
+        public StudentController(IHttpClientFactory clientFactory, IConfiguration configuration, ISRepository sRepo)
+        {
+            _clientFactory = clientFactory;
+            _configuration = configuration;
+            _sRepo = sRepo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ SwivelAcademyWEB/Controllers/TeacherController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SwivelAcademyWEB.Models;
+using SwivelAcademyWEB.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SwivelAcademyWEB.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        public static IConfiguration _configuration { get; set; }
+        private readonly ITRepository _tRepo;
+
+        public TeacherController(IHttpClientFactory clientFactory, IConfiguration configuration, ITRepository tRepo)
+        {
+            _clientFactory = clientFactory;
+            _configuration = configuration;
+            _tRepo = tRepo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult Course()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Profile(TeacherModel teacherModel)
+        {
+            return View();
+        }
+    }
+}
