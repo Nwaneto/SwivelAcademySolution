2b16a30756e42b1a5ea1e1dc3b01ac8195654eba
Chidi Nwaneto <nwanetochidiebere@gmail.com>
2021-06-30 08:20:56 +0100

    Update SwivelAcademyDB.sql

--- SwivelAcademyDB.sql
+++ SwivelAcademyDB.sql
@@ -1,5 +1,11 @@
 CREATE DATABASE [SwivelAcademyDB]
-
+GO
+USE [SwivelAcademyDB]
+GO
+/****** Object:  Table [dbo].[Courses]    Script Date: 30/06/2021 08:17:56 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Courses](
 	[CourseID] [int] IDENTITY(1,1) NOT NULL,
@@ -11,7 +17,7 @@ CREATE TABLE [dbo].[Courses](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Students]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  Table [dbo].[Students]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -28,7 +34,7 @@ CREATE TABLE [dbo].[Students](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[tbl_Student_Teacher_Course]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  Table [dbo].[tbl_Student_Teacher_Course]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -43,7 +49,7 @@ CREATE TABLE [dbo].[tbl_Student_Teacher_Course](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[tbl_Teacher_Course]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  Table [dbo].[tbl_Teacher_Course]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -58,7 +64,7 @@ CREATE TABLE [dbo].[tbl_Teacher_Course](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Teachers]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  Table [dbo].[Teachers]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -74,6 +80,64 @@ CREATE TABLE [dbo].[Teachers](
 	[TeacherID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+SET IDENTITY_INSERT [dbo].[Courses] ON 
+GO
+INSERT [dbo].[Courses] ([CourseID], [CourseTitle], [CourseSyllable]) VALUES (1, N'Biology', N'Something new in biology')
+GO
+INSERT [dbo].[Courses] ([CourseID], [CourseTitle], [CourseSyllable]) VALUES (2, N'Physics', N'Something New')
+GO
+INSERT [dbo].[Courses] ([CourseID], [CourseTitle], [CourseSyllable]) VALUES (3, N'Mathematics', N'Something New in Mathematics')
+GO
+SET IDENTITY_INSERT [dbo].[Courses] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Students] ON 
+GO
+INSERT [dbo].[Students] ([StudentID], [FirstName], [LastName], [Address], [Gender]) VALUES (1, N'Chidiebere', N'Nwaneto', N'Ajah, Lagos', N'Male')
+GO
+INSERT [dbo].[Students] ([StudentID], [FirstName], [LastName], [Address], [Gender]) VALUES (1002, N'werwtrt', N'rtretwert', N'sdgdfg', N'Female')
+GO
+INSERT [dbo].[Students] ([StudentID], [FirstName], [LastName], [Address], [Gender]) VALUES (1003, N'string', N'string', N'string', N'string')
+GO
+SET IDENTITY_INSERT [dbo].[Students] OFF
+GO
+SET IDENTITY_INSERT [dbo].[tbl_Student_Teacher_Course] ON 
+GO
+INSERT [dbo].[tbl_Student_Teacher_Course] ([ID], [StudentID], [Teacher_CourseID]) VALUES (1, 1, 1)
+GO
+INSERT [dbo].[tbl_Student_Teacher_Course] ([ID], [StudentID], [Teacher_CourseID]) VALUES (2, 1, 2)
+GO
+INSERT [dbo].[tbl_Student_Teacher_Course] ([ID], [StudentID], [Teacher_CourseID]) VALUES (3, 1, 3)
+GO
+INSERT [dbo].[tbl_Student_Teacher_Course] ([ID], [StudentID], [Teacher_CourseID]) VALUES (1002, 1003, 1)
+GO
+SET IDENTITY_INSERT [dbo].[tbl_Student_Teacher_Course] OFF
+GO
+SET IDENTITY_INSERT [dbo].[tbl_Teacher_Course] ON 
+GO
+INSERT [dbo].[tbl_Teacher_Course] ([ID], [TeacherID], [CourseID]) VALUES (1, 1, 1)
+GO
+INSERT [dbo].[tbl_Teacher_Course] ([ID], [TeacherID], [CourseID]) VALUES (2, 1, 2)
+GO
+INSERT [dbo].[tbl_Teacher_Course] ([ID], [TeacherID], [CourseID]) VALUES (3, 1, 3)
+GO
+INSERT [dbo].[tbl_Teacher_Course] ([ID], [TeacherID], [CourseID]) VALUES (4, 2, 1)
+GO
+INSERT [dbo].[tbl_Teacher_Course] ([ID], [TeacherID], [CourseID]) VALUES (5, 2, 2)
+GO
+INSERT [dbo].[tbl_Teacher_Course] ([ID], [TeacherID], [CourseID]) VALUES (6, 2, 3)
+GO
+INSERT [dbo].[tbl_Teacher_Course] ([ID], [TeacherID], [CourseID]) VALUES (1008, 2, 1)
+GO
+SET IDENTITY_INSERT [dbo].[tbl_Teacher_Course] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Teachers] ON 
+GO
+INSERT [dbo].[Teachers] ([TeacherID], [FirstName], [LastName], [Address], [Gender]) VALUES (1, N'Lydia', N'Ikoba', N'Lagos, Nigeria', N'Female')
+GO
+INSERT [dbo].[Teachers] ([TeacherID], [FirstName], [LastName], [Address], [Gender]) VALUES (2, N'John', N'Deen', N'Abuja, Nigeria', N'Male')
+GO
+SET IDENTITY_INSERT [dbo].[Teachers] OFF
 GO
 ALTER TABLE [dbo].[tbl_Student_Teacher_Course]  WITH CHECK ADD  CONSTRAINT [FK_tbl_Student_Teacher_Course_Students] FOREIGN KEY([StudentID])
 REFERENCES [dbo].[Students] ([StudentID])
@@ -95,7 +159,7 @@ REFERENCES [dbo].[Teachers] ([TeacherID])
 GO
 ALTER TABLE [dbo].[tbl_Teacher_Course] CHECK CONSTRAINT [FK_tbl_Teacher_Course_Teachers]
 GO
-/****** Object:  StoredProcedure [dbo].[STP_AssignTeacherToCourse]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  StoredProcedure [dbo].[STP_AssignTeacherToCourse]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -125,7 +189,7 @@ BEGIN
 	SELECT @Result AS Result
 END
 GO
-/****** Object:  StoredProcedure [dbo].[STP_CreateCourse]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  StoredProcedure [dbo].[STP_CreateCourse]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -157,7 +221,7 @@ BEGIN
 	SELECT @Result AS Result
 END
 GO
-/****** Object:  StoredProcedure [dbo].[STP_CreateStudent]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  StoredProcedure [dbo].[STP_CreateStudent]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -192,7 +256,7 @@ BEGIN
 	SELECT @Result AS Result
 END
 GO
-/****** Object:  StoredProcedure [dbo].[STP_CreateTeacher]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  StoredProcedure [dbo].[STP_CreateTeacher]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -227,7 +291,7 @@ BEGIN
 	SELECT @Result AS Result
 END
 GO
-/****** Object:  StoredProcedure [dbo].[STP_DeleteCourse]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  StoredProcedure [dbo].[STP_DeleteCourse]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -248,7 +312,7 @@ BEGIN
         WHERE  CourseID = @CourseId  
 END
 GO
-/****** Object:  StoredProcedure [dbo].[STP_DeleteStudent]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  StoredProcedure [dbo].[STP_DeleteStudent]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -269,7 +333,7 @@ BEGIN
         WHERE  StudentID = @StudentId  
 END
 GO
-/****** Object:  StoredProcedure [dbo].[STP_DeleteTeacher]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  StoredProcedure [dbo].[STP_DeleteTeacher]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -290,7 +354,7 @@ BEGIN
         WHERE  TeacherID = @TeacherId  
 END
 GO
-/****** Object:  StoredProcedure [dbo].[STP_GetAllCourses]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  StoredProcedure [dbo].[STP_GetAllCourses]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -311,7 +375,55 @@ BEGIN
 	from Courses 
 END
 GO
-/****** Object:  StoredProcedure [dbo].[STP_GetAllStudents]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  StoredProcedure [dbo].[STP_GetAllCoursesRegByStudent]    Script Date: 30/06/2021 08:17:56 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+-- =============================================
+-- Author:		<Chidiebere Nwaneto>
+-- Create date: <24th June, 2021>
+-- Description:	<Stored Procedure get all courses registered by a student>
+-- =============================================
+CREATE PROCEDURE [dbo].[STP_GetAllCoursesRegByStudent]
+	@StudentId INT
+	AS
+BEGIN
+	SELECT 
+		c.CourseID, CourseTitle, CourseSyllable, s.FirstName as TeacherName
+	from Courses c
+		INNER JOIN tbl_Student_Teacher_Course t ON c.CourseID = t.Teacher_CourseID
+		LEFT OUTER JOIN Teachers s ON s.TeacherID = t.ID
+	WHERE StudentID = @StudentId
+END
+GO
+/****** Object:  StoredProcedure [dbo].[STP_GetAllCoursesTaughtByTeacher]    Script Date: 30/06/2021 08:17:56 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+-- =============================================
+-- Author:		<Chidiebere Nwaneto>
+-- Create date: <24th June, 2021>
+-- Description:	<Stored Procedure get all courses taught by a teacher>
+-- =============================================
+CREATE PROCEDURE [dbo].[STP_GetAllCoursesTaughtByTeacher]
+	@TeacherId INT
+	AS
+BEGIN
+	SELECT 
+		c.CourseID, CourseTitle, CourseSyllable
+	from Courses c
+		INNER JOIN tbl_Teacher_Course t ON c.CourseID = t.CourseID
+	WHERE TeacherID = @TeacherId
+END
+GO
+/****** Object:  StoredProcedure [dbo].[STP_GetAllStudents]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -334,7 +446,7 @@ BEGIN
 	from Students 
 END
 GO
-/****** Object:  StoredProcedure [dbo].[STP_GetAllTeachers]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  StoredProcedure [dbo].[STP_GetAllTeachers]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -357,7 +469,7 @@ BEGIN
 	from Teachers 
 END
 GO
-/****** Object:  StoredProcedure [dbo].[STP_GetCourse]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  StoredProcedure [dbo].[STP_GetCourse]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -380,7 +492,7 @@ BEGIN
 	WHERE CourseID = @CourseId
 END
 GO
-/****** Object:  StoredProcedure [dbo].[STP_GetStudent]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  StoredProcedure [dbo].[STP_GetStudent]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -405,7 +517,7 @@ BEGIN
 	WHERE StudentID = @StudentId
 END
 GO
-/****** Object:  StoredProcedure [dbo].[STP_GetTeacher]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  StoredProcedure [dbo].[STP_GetTeacher]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -430,7 +542,7 @@ BEGIN
 	WHERE TeacherID = @TeacherId
 END
 GO
-/****** Object:  StoredProcedure [dbo].[STP_RegisterStudentToCourse]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  StoredProcedure [dbo].[STP_RegisterStudentToCourse]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -447,7 +559,7 @@ CREATE PROCEDURE [dbo].[STP_RegisterStudentToCourse]
 AS
 BEGIN
 	DECLARE @result VARCHAR(100) =''
-	INSERT INTO tbl_Teacher_Course
+	INSERT INTO tbl_Student_Teacher_Course
 	VALUES   
         (   
 			@StudentID,
@@ -461,7 +573,7 @@ BEGIN
 	SELECT @Result AS Result
 END
 GO
-/****** Object:  StoredProcedure [dbo].[STP_UpdateCourse]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  StoredProcedure [dbo].[STP_UpdateCourse]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -493,7 +605,7 @@ BEGIN
 	SELECT @Status AS Result
 END
 GO
-/****** Object:  StoredProcedure [dbo].[STP_UpdateStudent]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  StoredProcedure [dbo].[STP_UpdateStudent]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -529,7 +641,7 @@ BEGIN
 	SELECT @Status AS Result
 END
 GO
-/****** Object:  StoredProcedure [dbo].[STP_UpdateTeacher]    Script Date: 25/06/2021 12:37:01 ******/
+/****** Object:  StoredProcedure [dbo].[STP_UpdateTeacher]    Script Date: 30/06/2021 08:17:56 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -564,8 +676,3 @@ BEGIN
 
 	SELECT @Status AS Result
 END
-GO
-USE [master]
-GO
-ALTER DATABASE [SwivelAcademyDB] SET  READ_WRITE 
-GO
